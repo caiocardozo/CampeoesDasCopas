7ffc57db73c2bca4653a496998fb4d415d70b918
Caio Cardozo <caiocardozocb3@gmail.com>
2018-07-29 21:04:01 -0300

    Subindo o projeto

---
+++ CampeoesDasCopas/Controllers/GameTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GameTableViewCell.swift
+//  CampeoesDasCopas
+//
+//  Created by Caio Cardozo on 07/07/18.
+//  Copyright © 2018 Caio Cardozo. All rights reserved.
+//
+
+import UIKit
+
+class GameTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CampeoesDasCopas/Models/WorldCup.swift
@@ -0,0 +1,9 @@
+//
+//  WorldCup.swift
+//  CampeoesDasCopas
+//
+//  Created by Caio Cardozo on 05/07/18.
+//  Copyright © 2018 Caio Cardozo. All rights reserved.
+//
+
+import Foundation
